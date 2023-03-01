2494da034dec72b81552515008ba844e94aa1282
xzqt23 <angelo.agualada@gmail.com>
2023-03-01 12:09:28 +0800

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,2 +1,10 @@
 name = input ("What is your name? ")
 print(f"Hello, {name}!")
+print("Python","is the best", "programming language",sep = "->")
+####################################
+"""
+a = 12
+b = 13
+print(a,b)
+
+"""
